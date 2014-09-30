5b7ee53ed311513a60d16da5c2e14614dac752e0
tubefox_MAC <tubefoxjp@gmail.com>
2014-09-30 11:23:45 +0900

    マックからテスト

---
+++ UnityGitTest/Assets/script/frommac.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class frommac : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+
+				//from macだよ
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
